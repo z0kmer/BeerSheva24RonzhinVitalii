3be41515e42a27eb37c67e26accbb4c27976a3f1
Ronzin Vitaly <164080546+z0kmer@users.noreply.github.com>
2024-07-19 13:55:34 +0300

    Update BinaryArrays.java
    HomeWork_8

--- binarySearch/src/main/java/util/binaryarrays/BinaryArrays.java
+++ binarySearch/src/main/java/util/binaryarrays/BinaryArrays.java
@@ -1,6 +1,8 @@
 package util.binaryarrays;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.function.Predicate;
 
 public class BinaryArrays {
 
@@ -149,4 +151,126 @@ public class BinaryArrays {
 
         return res;
     }
+
+    /*
+     * Teachers version
+     */
+
+    public static boolean isOneSwapVTeacher(int[] array) {
+        // return true if a given array has exactly one swap to get sorted array
+        // the swaped array's elements may or may not be neighbors
+        boolean res = false;
+        int index1 = -1;
+        int index2 = 0;
+        index1 = getFirstIndex(array);
+        if (index1 > -1) {
+            index2 = getSecondIndex(array, index1);
+            res = isOneSwapCheck(array, index1, index2);
+        }
+        return res;
+
+    }
+    private static boolean isOneSwapCheck(int[] array, int index1, int index2) {
+        swap(array, index1, index2);
+        boolean res = isArraySorted(array);
+        swap(array, index2, index1); //array restored after swap
+        return res;
+    }
+    private static boolean isArraySorted(int[] array) {
+        int index = 1;
+        while (index < array.length && array[index] >= array[index - 1]) {
+            index++;
+        }
+        return index == array.length;
+    }
+    private static int getSecondIndex(int[] array, int index1) {
+        int index = array.length - 1;
+        int lowBorder = index1 + 1;
+        while (index > lowBorder && array[index] >= array[index1]) {
+            index--;
+        }
+        
+        return index;
+        
+    }
+    private static int getFirstIndex(int[] array) {
+        int index = 0;
+        int limit = array.length - 1;
+        while(index < limit && array[index] <= array[index + 1]) {
+            index++;
+        }
+        return index == limit ? -1 : index;
+    }
+    public static <T> void sort(T[] array, Comparator<T> comparator) {
+        int length = array.length;
+        boolean flSort = false;
+        do {
+            length--;
+            flSort = true;
+            for(int i = 0; i < length; i++) {
+                if(comparator.compare(array[i], array[i + 1]) > 0) {
+                    swap(array, i, i + 1);
+                    flSort = false;
+                }
+            }
+        }while(!flSort);
+    }
+    private static <T> void swap(T[] array, int i, int j) {
+        T tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
+
+    }
+
+    /*
+     * BinarySearch
+     */
+
+    public static <T> int binarySearch(T[] array, T key, Comparator<T> comp) {
+        int left = 0;
+        int right = array.length - 1;
+        int middle = (left + right) / 2;
+        int compRes = 0;
+        while (left <= right && (compRes = comp.compare(key, array[middle])) != 0) {
+            if (compRes < 0) {
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            }
+            middle = (left + right) / 2;
+        }
+        return left > right ? -(left + 1) : middle;
+    }
+
+    public static <T> int binarySearch(T[] array, T key) {
+        //The code should be base on binarySearch
+        return Arrays.binarySearch(array, key);
+    }
+
+    /*
+     * Insert, Find, removeIf
+     */
+
+    public static <T> T[] insert(T [] array, int index, T item) {
+        T[] res = java.util.Arrays.copyOf(array, array.length + 1);
+        System.arraycopy(array, index, res, index + 1, array.length - index);
+        res[index] = item;
+        return res;
+    }
+
+    public static <T> T[] find(T[]array, Predicate<T> predicate) {
+        T[] result = java.util.Arrays.copyOf(array, 0);
+        for(int i = 0; i < array.length; i++) {
+            if(predicate.test(array[i])) {
+                result = insert(result, result.length, array[i]);
+            }
+        }
+        return result;
+    }
+    
+    public static <T> T[] removeIf(T[] array, Predicate<T> predicate) {
+        //using oneline
+        return find(array, predicate.negate());
+    }
+    
 }
