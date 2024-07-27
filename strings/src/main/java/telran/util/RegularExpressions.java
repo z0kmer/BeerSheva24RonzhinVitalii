0a7c4eeccbdb14fe1c22eb5f99f3b1e305d33d6b
Ronzin Vitaly <164080546+z0kmer@users.noreply.github.com>
2024-07-28 00:30:02 +0300

    Add files via upload
    HomeWork_10
    RegExp

---
+++ strings/src/main/java/telran/util/RegularExpressions.java
@@ -0,0 +1,16 @@
+package telran.util;
+
+public class RegularExpressions {
+    public static String firstName() {
+        //regex for strings starting with capital letter and rest as lowercase letters
+        //minimal length is 5 letters
+        return "[A-Z][a-z]{4,}";
+    }
+
+    public static String javaVariable() {
+        //regular expression for testing syntax of Java variable names
+        //only ACII symbols are allowed
+        return "^[a-z][a-zA-Z_$0-9]*|^[_$][a-zA-Z_$0-9]{2,}";
+    }
+
+}
---
+++ strings/src/test/java/telran/util/RegularExpressionsTest.java
@@ -0,0 +1,34 @@
+package telran.util;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class RegularExpressionsTest {
+    
+    @Test
+    void TestFirstNames() {
+        String result = RegularExpressions.firstName();
+        String[] arraysTrue = {"Alice", "Bobby", "Charlie"};
+        String[] arraysFalse = {"eve", "123", "John", "Сашуня"};
+    for(int i = 0; i < arraysTrue.length; i++) {
+        assertTrue(arraysTrue[i].matches(result));
+    }
+    for(int i = 0; i < arraysFalse.length; i++) {
+        assertFalse(arraysFalse[i].matches(result));
+    }
+    }
+
+    @Test
+    void testValidJavaVariable() {
+        String result = RegularExpressions.javaVariable();
+        String[] arraysTrue = {"myVar", "m", "_myVar", "$myyVar"};
+        String[] arraysFalse = {"123Var", "my Var", "myVar!", "_", "cашуня", ""};
+    for(int i = 0; i < arraysTrue.length; i++) {
+        assertTrue(arraysTrue[i].matches(result));
+    }
+    for(int i = 0; i < arraysFalse.length; i++) {
+        assertFalse(arraysFalse[i].matches(result));
+    }
+    }
+}
