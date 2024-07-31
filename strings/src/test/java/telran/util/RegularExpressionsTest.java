32b8da00b36dc100a727beab94cd9f60d6e9598b
Ronzin Vitaly <164080546+z0kmer@users.noreply.github.com>
2024-07-31 19:52:59 +0300

    Update RegularExpressionsTest.java
    HomeWork_11
    regex & checking the expression that it meets the requirements of mathematics and Java

--- strings/src/test/java/telran/util/RegularExpressionsTest.java
+++ strings/src/test/java/telran/util/RegularExpressionsTest.java
@@ -1,8 +1,10 @@
 package telran.util;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import static telran.util.RegularExpressions.*;
+
 import org.junit.jupiter.api.Test;
+
 
 public class RegularExpressionsTest {
     
@@ -31,4 +33,70 @@ public class RegularExpressionsTest {
         assertFalse(arraysFalse[i].matches(result));
     }
     }
+
+
+    @Test
+    public void testisArithmeticExpression() {
+        assertTrue(isArithmeticExpression("2 * (3 + 2 / (7 - 2) * (4 - 0) / 2)"));
+        assertTrue(isArithmeticExpression("abrgal * (3 + 2 / (7 - 2) * (4 - 0) / 2)"));
+        assertTrue(isArithmeticExpression("2 * (qwex + 2 / (7 - 2) * (4 - 0) / 2)"));
+        assertTrue(isArithmeticExpression("2 * (__ + 2 / (7 - $) * (4 - 0) / 2)"));
+    
+        assertFalse(isArithmeticExpression("("));
+        assertFalse(isArithmeticExpression(")"));
+        assertFalse(isArithmeticExpression("((())"));
+        assertFalse(isArithmeticExpression("2 (3 + b/ ( 7-2)* (4-0) / 2 )"));
+        assertFalse(isArithmeticExpression("2 (3 + b)"));
+        assertFalse(isArithmeticExpression("(3 + b)2"));
+        assertFalse(isArithmeticExpression("(3 + b) 2"));
+        assertFalse(isArithmeticExpression("2*(+3 + b)"));
+        assertFalse(isArithmeticExpression("2*( +3 + b)"));
+        assertFalse(isArithmeticExpression("2*( +3 + abstract)"));
+        assertFalse(isArithmeticExpression("(3 + b+)*2"));
+        assertFalse(isArithmeticExpression("(3 + b+ ) *2"));
+        assertFalse(isArithmeticExpression("(3 + b+ ) *_"));
+        assertFalse(isArithmeticExpression("2 * (3 + b/ 2( 7-2) (4-0) / 2 )"));
+    }
+
+    @Test
+    public void testisValidbrackets() {
+        assertTrue(isBracketsBalanced("()"));
+        assertTrue(isBracketsBalanced("2*(3+b/(7-2)*(4-0)/2)"));
+    
+        assertFalse(isBracketsBalanced("("));
+
+        assertFalse(isBracketsBalanced(")"));
+        assertFalse(isBracketsBalanced("((())"));
+    }
+
+    @Test
+    public void testValidateExpression() {
+        assertTrue(isBracketsBalanced("()"));
+        assertTrue(validateExpression("(6)"));
+        assertTrue(validateExpression("2*(3+b/(7-2)*(4-0)/2)"));
+    
+        assertFalse(validateExpression(")("));
+
+        assertFalse(validateExpression("2(3+b/(7-2)*(4-0)/2)"));
+        assertFalse(validateExpression("2(3+b)"));
+        assertFalse(validateExpression("2*(+3+b)"));
+        assertFalse(validateExpression("2*(3+b/2(7-2)(4-0)/2)"));
+
+        assertFalse(isArithmeticExpression("(3+b)2"));
+        assertFalse(isArithmeticExpression("(3+b+)*2"));
+    }
+
+    @Test
+    public void testIsValidElements() {
+        assertTrue(isValidElements("(6)"));
+        assertTrue(isValidElements("2*(3+b/(7-2)*(4-0)/2)"));
+
+        assertTrue(isValidElements("2(3+bbb/(7-2)*(4-0)/2)"));
+        assertTrue(isValidElements("2(3+b)"));
+        assertTrue(isValidElements("2*(+3+_b)"));
+        assertTrue(isValidElements("2*(3+b/2(7-2)(4-0)/2)"));
+
+        assertTrue(isValidElements("(3+b)2"));
+    }
+    
 }
