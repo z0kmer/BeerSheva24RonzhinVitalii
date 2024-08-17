d8cbc5fcebe7a0acf66144cf056d68921222ea13
Ronzin Vitaly <164080546+z0kmer@users.noreply.github.com>
2024-08-17 23:34:47 +0300

    Update Range.java
    HW_14
    HW_18

--- range-exceptions-iterator/range/src/main/java/telran/range/Range.java
+++ range-exceptions-iterator/range/src/main/java/telran/range/Range.java
@@ -7,41 +7,47 @@ import java.util.function.Predicate;
 import telran.range.exceptions.OutOfRangeMaxValueException;
 import telran.range.exceptions.OutOfRangeMinValueException;
 
-public class Range implements Iterable<Integer>{
+public class Range implements Iterable<Integer> {
     private static final String ERROR_MESSAGE = "max less or equal min";
     private int min;
     private int max;
     private Predicate<Integer> predicate;
+
     private Range(int min, int max) {
         this.min = min;
         this.max = max;
     }
+
     public static Range getRange(int min, int max) {
         if (max <= min) {
             throw new IllegalArgumentException(ERROR_MESSAGE);
         }
         return new Range(min, max);
     }
-    void setPredicate(Predicate<Integer> predicate) {
+
+    public void setPredicate(Predicate<Integer> predicate) {
         this.predicate = predicate;
     }
+
     public void checkNumber(int number) throws OutOfRangeMaxValueException, OutOfRangeMinValueException {
-        if(number > max) {
+        if (number > max) {
             throw new OutOfRangeMaxValueException(max, number);
         }
         if (number < min) {
             throw new OutOfRangeMinValueException(min, number);
         }
     }
+
     @Override
     public Iterator<Integer> iterator() {
         return new RangeIterator();
     }
+
     private class RangeIterator implements Iterator<Integer> {
-        int current;
-        
-        public RangeIterator() {
-            this.current = min;
+        private int current;
+
+        private RangeIterator() {
+            setCurrent(min);
         }
 
         @Override
@@ -54,17 +60,18 @@ public class Range implements Iterable<Integer>{
             if (!hasNext()) {
                 throw new NoSuchElementException();
             }
-            int temp = current;
-            setCurrent();
-            return temp;
+            int value = current;
+            setCurrent(current + 1);
+            return value;
         }
-    
-        private void setCurrent() {
-            while (current <= max) {
-                if (predicate == null || predicate.test(current)) {
-                    return;
+        
+        private void setCurrent(int start) {
+            current = start;
+            if (predicate != null) {
+                while (current <= max && !predicate.test(current)) {
+                    current++;
                 }
-                current++;
             }
         }
     }
+}
