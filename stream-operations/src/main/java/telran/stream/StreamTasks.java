b1be0d65023a0b595c6bd364d4be58e7f06058c8
Ronzin Vitaly <164080546+z0kmer@users.noreply.github.com>
2024-08-18 20:26:28 +0300

    Update StreamTasks.java
    HW_19
    shuffle the numbers

--- stream-operations/src/main/java/telran/stream/StreamTasks.java
+++ stream-operations/src/main/java/telran/stream/StreamTasks.java
@@ -7,10 +7,7 @@ public class StreamTasks {
     public static int[] shuffle(int [] arr){
         //how to int to stream in java =)) use this metod
         //only one pipeline for getting new array of int whith all numbers from a given array but with different order each method call returns new array in some random order
-
-        if (arr == null || arr.length == 0) {
-            return new int[0];
-        }
+        
         Random random = new Random();
         return IntStream.of(arr)
                         .boxed()
