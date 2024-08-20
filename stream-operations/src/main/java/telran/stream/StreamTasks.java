be8ac3d958c81adf8714eb1ee334bee9d5771955
Ronzin Vitaly <164080546+z0kmer@users.noreply.github.com>
2024-08-20 12:58:24 +0300

    Update StreamTasks.java
    HW_19

--- stream-operations/src/main/java/telran/stream/StreamTasks.java
+++ stream-operations/src/main/java/telran/stream/StreamTasks.java
@@ -9,16 +9,13 @@ public class StreamTasks {
         //only one pipeline for getting new array of int whith all numbers from a given array but with different order each method call returns new array in some random order
         
         Random random = new Random();
-        return IntStream.range(0, arr.length)
-                .map(i -> {
-                    int randomIndex = random.nextInt(arr.length);
-                    int temp = arr[i];
-                    arr[i] = arr[randomIndex];
-                    arr[randomIndex] = temp;
-                    return i;
-                })
-                .mapToObj(i -> arr)
-                .findFirst()
-                .orElse(arr);
+        return Optional.of(arr)
+                    .filter(a -> a.length > 0)
+                    .map(a -> random.ints(0, a.length)
+                                    .distinct()
+                                    .limit(a.length)
+                                    .map(i -> a[i])
+                                    .toArray())
+                    .orElse(new int[0]);
     }
 }
