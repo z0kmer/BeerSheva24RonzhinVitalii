6c7b79c53008529d0b1294ac8c51d562165d5c63
Ronzin Vitaly <164080546+z0kmer@users.noreply.github.com>
2024-08-22 17:25:23 +0300

    Add files via upload
    HW_21
    Writing all methods of the class LinkedList

---
+++ telran-collections-framework/src/test/java/telran/util/LinkedListTest.java
@@ -0,0 +1,13 @@
+package telran.util;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class LinkedListTest extends ListTest{
+    
+    @BeforeEach
+    @Override
+    void setUp() {
+        collection = new LinkedList<>();
+        super.setUp();
+    }
+}
