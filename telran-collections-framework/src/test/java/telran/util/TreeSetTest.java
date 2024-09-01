48643c0372e2cd45401805e85bf2976553f7a5e9
Ronzin Vitaly <164080546+z0kmer@users.noreply.github.com>
2024-09-01 19:35:53 +0300

    Add files via upload
    HW_23

---
+++ telran-collections-framework/src/test/java/telran/util/TreeSetTest.java
@@ -0,0 +1,25 @@
+package telran.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class TreeSetTest extends SetTest{
+    @BeforeEach
+    @Override
+    void setUp() {
+        collection = new TreeSet<>();
+        super.setUp();
+    }
+    @Override
+    protected void runTest(Integer[] expected) {
+        Integer[] expectedSorted = Arrays.copyOf(expected, expected.length);
+        Arrays.sort(expectedSorted);
+        Integer[] actual = collection.stream().toArray(Integer[]::new);
+        
+        assertArrayEquals(expectedSorted, actual);
+        assertEquals(expected.length, collection.size());
+    }
+}
