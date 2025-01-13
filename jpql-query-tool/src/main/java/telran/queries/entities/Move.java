acc36a15003aa22fabb910de9a6aecd6444de7b2
Ronzin Vitaly <164080546+z0kmer@users.noreply.github.com>
2025-01-13 22:49:03 +0200

    Game_AWS

--- jpql-query-tool/src/main/java/telran/queries/entities/Move.java
+++ jpql-query-tool/src/main/java/telran/queries/entities/Move.java
@@ -1,28 +1,65 @@
 package telran.queries.entities;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-@Table(name="move")
+
 @Entity
 public class Move {
-@Id
-@GeneratedValue
-long id;
-int bulls;
-int cows;
-String sequence;
-@ManyToOne
-@JoinColumn (name = "game_gamer_id")
-GameGamer gameGamer;
-@Override
-public String toString() {
-    return "Move [id=" + id + ", bulls=" + bulls + ", cows=" + cows + ", sequence=" + sequence + ", gameGamer="
-            + gameGamer.id + "]";
-}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    private String sequence;
+    private int bulls;
+    private int cows;
 
+    @ManyToOne
+    @JoinColumn(name = "game_gamer_id")
+    private GameGamer gameGamer;
 
+    // Геттеры и сеттеры
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSequence() {
+        return sequence;
+    }
+
+    public void setSequence(String sequence) {
+        this.sequence = sequence;
+    }
+
+    public int getBulls() {
+        return bulls;
+    }
+
+    public void setBulls(int bulls) {
+        this.bulls = bulls;
+    }
+
+    public int getCows() {
+        return cows;
+    }
+
+    public void setCows(int cows) {
+        this.cows = cows;
+    }
+
+    public GameGamer getGameGamer() {
+        return gameGamer;
+    }
+
+    public void setGameGamer(GameGamer gameGamer) {
+        this.gameGamer = gameGamer;
+    }
 }
