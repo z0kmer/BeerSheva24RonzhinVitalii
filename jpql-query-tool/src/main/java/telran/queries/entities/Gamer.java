f6e9006914a94c4d3a4fd9234ba3037a2f8a75d0
Ronzin Vitaly <164080546+z0kmer@users.noreply.github.com>
2025-01-13 22:49:33 +0200

    Game_AWS

--- jpql-query-tool/src/main/java/telran/queries/entities/Gamer.java
+++ jpql-query-tool/src/main/java/telran/queries/entities/Gamer.java
@@ -1,18 +1,31 @@
 package telran.queries.entities;
+
 import java.time.LocalDate;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+
 @Entity
-@Table(name="gamer")
 public class Gamer {
     @Id
-    String username;
-    LocalDate birthdate;
-    @Override
-    public String toString() {
-        return "Gamer [username=" + username + ", birthdate=" + birthdate + "]";
+    private String username;
+    private LocalDate birthdate;
+
+    // Геттеры и сеттеры
+
+    public String getUsername() {
+        return username;
     }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
 }
