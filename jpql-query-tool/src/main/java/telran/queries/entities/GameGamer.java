b5bb5f09613f7bc25bedcecc0c500aac68bbec1b
Ronzin Vitaly <164080546+z0kmer@users.noreply.github.com>
2025-01-13 22:50:14 +0200

    Game_AWS

--- jpql-query-tool/src/main/java/telran/queries/entities/GameGamer.java
+++ jpql-query-tool/src/main/java/telran/queries/entities/GameGamer.java
@@ -1,28 +1,63 @@
 package telran.queries.entities;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+
 @Entity
-@Table(name="game_gamer")
+@Table(name = "game_gamer") // Указываем название таблицы
 public class GameGamer {
     @Id
-    @GeneratedValue
-    long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     @ManyToOne
-    @JoinColumn(name="game_id")
-    Game game;
+    @JoinColumn(name = "game_id")
+    private Game game;
+
     @ManyToOne
     @JoinColumn(name = "gamer_id")
-    Gamer gamer;
-    @Column(name = "is_winner")
-    boolean isWinner;
-    @Override
-    public String toString() {
-        return "GameGamer [id=" + id + ", game=" + game.id + ", gamer=" + gamer.username + ", isWinner=" + isWinner + "]";
+    private Gamer gamer;
+
+    @Column(name = "is_winner", nullable = false)
+    private Boolean isWinner = false;
+
+    // Геттеры и сеттеры
+
+    public Long getId() {
+        return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public Gamer getGamer() {
+        return gamer;
+    }
+
+    public void setGamer(Gamer gamer) {
+        this.gamer = gamer;
+    }
+
+    public Boolean getIsWinner() {
+        return isWinner;
+    }
+
+    public void setIsWinner(Boolean isWinner) {
+        this.isWinner = isWinner;
+    }
 }
