63999817e44835c9316e930dcf0e9952006342b7
z0kmer <cyberelf31@gmail.com>
2025-01-23 14:50:53 +0200

    Objects =)

--- Node-Project/js-introduction/test/objects.test.mjs
+++ Node-Project/js-introduction/test/objects.test.mjs
@@ -1,43 +1,20 @@
 import { describe, expect, it } from 'vitest';
-//import getOccurencesObject from '../objects.mjs';
 
-describe("object iteratiting", () => {
-    const person = {name: "Vasya", age: 25}
-    it("for-in iterating", () => {
-        const keys = ["name", "age"];
-        const actual = [];
-        let index = 0;
-        for(let key in person) {
-            actual[index++] = key;
-        }
-        expect(actual).toEqual(keys);
+describe ("Object copying", () => {
+    const person1 = {name: "Vasya", age: 25};
+    it("showing example of referances assigment but not copy", () => {
+        const person2 = person1;
+        person2.gender = "mail";
+        expect(person1.gender).toBe("mail");
     })
-    it("keys iterating usin for...of", () => {
-        const keys = ["name", "age"];
-        const actual = [];
-        let index = 0;
-        for(let key of Object.keys(person)) {
-            actual[index++] = key;
-        }
-        expect(actual).toEqual(keys);
+    it("copying using method assign of class Object", () => {
+        const person2 = {...person1};
+        person2.city = "Lod";
+        expect(person1.city).toBeUndefined;
+        person1.city = "Lod";
+        expect(person1 != person2).toBeTruthy();
+        expect(person2).toEqual(person1);
+        expect(person2).not.toBe(person1);
     })
-    it("values iterating using for...of constructions", () => {
-        const expectedValues = ["Vasya", 25];
-        const actualValues = [];
-        let index = 0;
-        for(let value of Object.values(person)) {
-            actualValues[index++] = value;
-        }
-        expect(actualValues).toEqual(expectedValues);
-    })
-    it("iterating entries using ofor...f construction", () => {
-        const expectedEntries = [["name", "Vasya"], ["age", 25]];
-        const actualEntries = [];
-        let index = 0;
-        for(let entity of Object.entries(person)) {
-            actualEntries[index++] = entity;
-        }
-        expect(actualEntries).toEqual(expectedEntries);
-    })
+
 })
-
