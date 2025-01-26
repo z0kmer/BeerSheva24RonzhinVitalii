47c4959de199de5daa07c47a357bec60a8b5bc0d
z0kmer <cyberelf31@gmail.com>
2025-01-26 12:58:13 +0200

    removing

--- Node-Project/js-introduction/test/arrays.test.mjs
+++ Node-Project/js-introduction/test/arrays.test.mjs
@@ -1,23 +1,44 @@
 import { describe, expect, it } from 'vitest';
 
-describe ("inserting new elments in array", () => {
+// describe ("inserting new elments in array", () => {
+//     const insertedNumbers = [-10, -20];
+//     it("inserting at begining of array", () => {
+//         const ar = [1, 2];
+//         const expected = [-10, -20, 1, 2];
+//         ar.unshift(...insertedNumbers);
+//         expect(ar).toEqual(expected);
+//     })
+//     it("inserting at end of array", () => {
+//         const ar = [1, 2];
+//         const expected = [1, 2, -10, -20];
+//         ar.push(...insertedNumbers);
+//         expect(ar).toEqual(expected);
+//     })
+//     it("inserting at midle of array", () => {
+//         const ar = [1, 2];
+//         const expected = [1, -10, -20, 2];
+//         ar.splice(1, 0, ...insertedNumbers);
+//         expect(ar).toEqual(expected);
+//     })
+// })
+describe ("removing elements from array", () => {
     const insertedNumbers = [-10, -20];
-    it("inserting at begining of array", () => {
-        const ar = [1, 2];
-        const expected = [-10, -20, 1, 2];
-        ar.unshift(...insertedNumbers);
+    it("removing from begining of array", () => {
+        const ar = [1, 2, 3, 4];
+        const expected = [2, 3, 4];
+        ar.shift(ar);
         expect(ar).toEqual(expected);
     })
-    it("inserting at end of array", () => {
-        const ar = [1, 2];
-        const expected = [1, 2, -10, -20];
-        ar.push(...insertedNumbers);
+    it("removing from end of array", () => {
+        const ar = [1, 2, 3, 4];
+        const expected = [1, 2, 3];
+        ar.pop(ar);
         expect(ar).toEqual(expected);
     })
-    it("inserting at midle of array", () => {
-        const ar = [1, 2];
-        const expected = [1, -10, -20, 2];
-        ar.splice(1, 0, ...insertedNumbers);
+    it("removing from midle of array", () => {
+        const ar = [1, 2, 3, 4];
+        const expected = [1, 4];
+        ar.splice(1, 2);
         expect(ar).toEqual(expected);
     })
 })
