ba23be1a14151ba7cf697e416d3e494e0e32381d
z0kmer <cyberelf31@gmail.com>
2025-01-29 11:19:49 +0200

    array distructoring

--- Node-Project/js-introduction/test/arrays.test.mjs
+++ Node-Project/js-introduction/test/arrays.test.mjs
@@ -1,5 +1,4 @@
 import { describe, expect, it } from 'vitest';
-
 // describe ("inserting new elments in array", () => {
 //     const insertedNumbers = [-10, -20];
 //     it("inserting at begining of array", () => {
@@ -21,40 +20,57 @@ import { describe, expect, it } from 'vitest';
 //         expect(ar).toEqual(expected);
 //     })
 // })
-describe ("removing elements from array", () => {
-    const insertedNumbers = [-10, -20];
-    it("removing from begining of array", () => {
-        const ar = [1, 2, 3, 4];
-        const expected = [2, 3, 4];
-        ar.shift(ar);
-        expect(ar).toEqual(expected);
-    })
-    it("removing from end of array", () => {
-        const ar = [1, 2, 3, 4];
-        const expected = [1, 2, 3];
-        ar.pop(ar);
-        expect(ar).toEqual(expected);
-    })
-    it("removing from midle of array", () => {
-        const ar = [1, 2, 3, 4];
-        const expected = [1, 4];
-        ar.splice(1, 2);
-        expect(ar).toEqual(expected);
-    })
-})
-//===============
-import { myMap, myReduce } from '../arrays.mjs';
+// describe ("removing elements from array", () => {
+//     const insertedNumbers = [-10, -20];
+//     it("removing from begining of array", () => {
+//         const ar = [1, 2, 3, 4];
+//         const expected = [2, 3, 4];
+//         ar.shift(ar);
+//         expect(ar).toEqual(expected);
+//     })
+//     it("removing from end of array", () => {
+//         const ar = [1, 2, 3, 4];
+//         const expected = [1, 2, 3];
+//         ar.pop(ar);
+//         expect(ar).toEqual(expected);
+//     })
+//     it("removing from midle of array", () => {
+//         const ar = [1, 2, 3, 4];
+//         const expected = [1, 4];
+//         ar.splice(1, 2);
+//         expect(ar).toEqual(expected);
+//     })
+// })
+// //===============
+// import { myMap, myReduce } from '../arrays.mjs';
+// const array = [10, 1000, -10, 30, 60];
+// describe("map / reduce", () => {
+//     array.map = myMap;
+//     array.reduce = myReduce;
+//     it("myMap testing", () => {
+//        const expected = [10, 1001, -8, 33, 64]//array of elements where each element will be converted to element + index
+//        expect(array.map((e,index) => e + index)).toEqual(expected);
+//     })
+//     it("myReduce testing", () => {
+//         //TODO
+//         //write two reduce methods for taking sum of all array numbers
+//         //and minimal and maximal values for only one reduce call
+//           })
+// })
+//=========================================
 const array = [10, 1000, -10, 30, 60];
-describe("map / reduce", () => {
-    array.map = myMap;
-    array.reduce = myReduce;
-    it("myMap testing", () => {
-       const expected = [10, 1001, -8, 33, 64]//array of elements where each element will be converted to element + index
-       expect(array.map((e,index) => e + index)).toEqual(expected);
+describe('destructing array', () => {
+    it('assigning two first numbers of of array to diffferent variables', () => {
+        const [a,b] = array;
+        expect(a).toBe(array[0]);
+        expect(b).toBe(array[1]);
     })
-    it("myReduce testing", () => {
-        //TODO
-        //write two reduce methods for taking sum of all array numbers
-        //and minimal and maximal values for only one reduce call
-          })
+    it('swap of values by using destructing', () => {
+        let a = 10, b = 20;
+        // const ar = [a, b];
+        // [b, a] = ar;
+        [b, a] = [a, b];
+        expect(a).toBe(20);
+        expect(b).toBe(10);
+    })
 })
