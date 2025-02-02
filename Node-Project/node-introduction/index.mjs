bc07dfea7849f34c666a6f473a1790f44e2d6fb0
z0kmer <cyberelf31@gmail.com>
2025-02-02 15:28:00 +0200

    winston_logging

---
+++ Node-Project/node-introduction/index.mjs
@@ -0,0 +1,9 @@
+
+import logger from './logger.mjs';
+//import path from 'path';
+//import { fileURLToPath } from 'url';
+
+console.log('from module', process.env.LEVEL);
+logger.info(logger.level);
+
+//===================
