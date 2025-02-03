58bde86d016ae2d5f0b170b94594a81eadc668f9
z0kmer <cyberelf31@gmail.com>
2025-02-03 11:30:00 +0200

    promises

--- Node-Project/node-introduction/index.mjs
+++ Node-Project/node-introduction/index.mjs
@@ -1,13 +1,7 @@
 
-//import logger from './logger.mjs';
-import fs from 'node:fs';
-const data = fs.readFileSync('./index.mjs', 'utf8', (err, data) => {
-    if(err) {
-        loggers.error(err);
-    } else {
-        console.log(data);
-    }
-});
-console.log(data);
-fs.writeFileSync('./file.txt', ["kuku", "kukureku", "Hello World!"]
-    .join('\n'), 'utf-16le', () => loggers.debug('file saved in utf-16le format'));
+import { readFile, writeFile } from 'node:fs/promises';
+import logger from './logger.mjs';
+const data = await readFile('./file.txt', 'utf-16le')
+console.log(data)
+writeFile('./file.txt', ["kuku", "kukureku", "Hello World!"].join('\n'), 'utf-16le')
+logger.info("function finished");
