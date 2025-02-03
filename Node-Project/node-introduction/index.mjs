92c5312421379b2ec219a6c66dd46f0071a49048
z0kmer <cyberelf31@gmail.com>
2025-02-03 11:05:30 +0200

    way to async

--- Node-Project/node-introduction/index.mjs
+++ Node-Project/node-introduction/index.mjs
@@ -1,7 +1,13 @@
 
 //import logger from './logger.mjs';
 import fs from 'node:fs';
-const data = fs.readFileSync('./index.mjs', 'utf8');
-// logger.info(data);
+const data = fs.readFileSync('./index.mjs', 'utf8', (err, data) => {
+    if(err) {
+        loggers.error(err);
+    } else {
+        console.log(data);
+    }
+});
 console.log(data);
-fs.writeFileSync('./file.txt', ["kuku", "kukureku", "Hello World!"].join('\n'), 'utf-16le')
+fs.writeFileSync('./file.txt', ["kuku", "kukureku", "Hello World!"]
+    .join('\n'), 'utf-16le', () => loggers.debug('file saved in utf-16le format'));
