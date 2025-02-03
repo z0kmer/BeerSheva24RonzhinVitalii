cf968c5011cc0fdaf8f1262e3ff557d4f33034d7
z0kmer <cyberelf31@gmail.com>
2025-02-03 11:32:43 +0200

    v/01

--- Node-Project/node-introduction/index.mjs
+++ Node-Project/node-introduction/index.mjs
@@ -1,7 +1,7 @@
 
 import { readFile, writeFile } from 'node:fs/promises';
 import logger from './logger.mjs';
-const data = await readFile('./file.txt', 'utf-16le')
+const data = await readFile('./file.txt','utf-16le')
 console.log(data)
-writeFile('./file.txt', ["kuku", "kukureku", "Hello World!"].join('\n'), 'utf-16le')
+writeFile('./file.txt', ["kuku", "kukureku", "שלום"].join('\n'), 'utf-16le')
 logger.info("function finished");
