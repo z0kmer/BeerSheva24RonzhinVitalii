655d7a1ced505f9083e3ed9c5590fa8bf1daa0c8
z0kmer <cyberelf31@gmail.com>
2025-02-03 10:34:53 +0200

    fs_synchronos

--- Node-Project/node-introduction/index.mjs
+++ Node-Project/node-introduction/index.mjs
@@ -1,5 +1,7 @@
 
+//import logger from './logger.mjs';
 import fs from 'node:fs';
-// const data = fs.readFileSync('./index.mjs');
+const data = fs.readFileSync('./index.mjs', 'utf8');
 // logger.info(data);
-fs.writeFileSync('./file.txt', ["kuku", "kukureku", "Hello World!"].join('\n'))
+console.log(data);
+fs.writeFileSync('./file.txt', ["kuku", "kukureku", "Hello World!"].join('\n'), 'utf-16le')
