ce5a26a47b94f73343da6befe1d982899ae2f45b
z0kmer <cyberelf31@gmail.com>
2025-02-05 12:06:24 +0200

    Events_Logger

--- Node-Project/node-introduction/logger.mjs
+++ Node-Project/node-introduction/logger.mjs
@@ -1,11 +1,29 @@
-import winston from 'winston';
+// import winston from 'winston';
 
-const logger = winston.createLogger({
-    level:process.env.LEVEL ?? 'info',
-    format: winston.format.simple(),
-    transports: [
-        new winston.transports.Console
-    ]
+// const logger = winston.createLogger({
+//     level:process.env.LEVEL ?? 'info',
+//     format: winston.format.simple(),
+//     transports: [
+//         new winston.transports.Console
+//     ]
 
-})
-export default logger;
+// })
+// export default logger;
+
+import { EventEmitter } from 'node:events';
+import winston from 'winston/lib/winston/config';
+
+class Logger extends EventEmitter {
+    #logger;
+    constructor() {
+        this.#logger = winston.createLogger({
+            level: process.env.LEVEL ?? "info",
+            format: winston.format.simple(),
+            transports: [new winston.transports.Console()],
+        });
+    }
+    log(level, message) {
+        this.#logger.log(level, message);
+        this.emit(level, message);
+    }
+}
