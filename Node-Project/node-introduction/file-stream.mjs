f6068b08dbc4418bbfe781f8071fc95fb07dd4fa
z0kmer <cyberelf31@gmail.com>
2025-02-06 12:29:40 +0200

    file streams (RW)

---
+++ Node-Project/node-introduction/file-stream.mjs
@@ -0,0 +1,5 @@
+import fs from 'node:fs';
+
+const stream = fs.ReadStream('./index.mjs');
+
+export default stream;
--- Node-Project/node-introduction/index.mjs
+++ Node-Project/node-introduction/index.mjs
@@ -1,4 +1,15 @@
-import stream from './sender-data.mjs';
-let res = '';
-stream.on('data', chunk => res += chunk);
-stream.on('end', () => console.log(res));
+// let res = '';
+// stream.on('data', chunk => res += chunk);
+// stream.on('end', () => console.log(res));
+import stream from './file-stream.mjs';
+import writableStream from './file-writable-stream.mjs';
+
+(async () => {
+    for await(const res of stream) {
+        console.log(res.toString());
+    }
+})()
+
+writableStream.write("Hello ");
+writableStream.write("World !!!");
+writableStream.end();
