c2bf7147609a12ead57d9274d43425ee4775be47
z0kmer <cyberelf31@gmail.com>
2025-02-10 15:30:42 +0200

    server education 1.0

--- Node-Project/express/index.js
+++ Node-Project/express/index.js
@@ -1,10 +1,10 @@
 import express from 'express';
 const courses = {
-    J101: {id: "J101", "name": "Front-End", lecturer: "Vasya", hours: 200}
-    J102: {id: "J102", "name": "JAVA", lecturer: "Vasya", hours: 300}
-    J103: {id: "J103", "name": "back-End", lecturer: "Olya", hours: 350}
-    J104: {id: "J104", "name": "Node", lecturer: "Olya", hours: 150}
-    J105: {id: "J105", "name": "AWS", lecturer: "Vova", hours: 200}
+    J101: {id: "J101", "name": "Front-End", lecturer: "Vasya", hours: 200},
+    J102: {id: "J102", "name": "JAVA", lecturer: "Vasya", hours: 300},
+    J103: {id: "J103", "name": "back-End", lecturer: "Olya", hours: 350},
+    J104: {id: "J104", "name": "Node", lecturer: "Olya", hours: 150},
+    J105: {id: "J105", "name": "AWS", lecturer: "Vova", hours: 200},
     J106: {id: "J106", "name": "C++", lecturer: "Vova", hours: 500}
 };
 const app = express();
