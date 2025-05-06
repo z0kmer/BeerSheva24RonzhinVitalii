1d2dda0c9f43de185f5bc1bea5e6b290872c32ae
z0kmer <cyberelf31@gmail.com>
2025-05-06 10:44:15 +0300

    adaptive Gallery whith txts

---
+++ Front-End-Projects/html-css-js-intro/Media/src/script.js
@@ -0,0 +1,14 @@
+function changeImage(imageSrc, textFile) {
+    document.getElementById('main-image').src = imageSrc;
+
+    fetch(textFile)
+        .then(response => response.text())
+        .then(text => {
+            document.getElementById('image-text').innerText = text;
+        })
+        .catch(error => console.error("Ошибка загрузки файла:", error));
+}
+
+function hideText() {
+    document.querySelector('.hover-text').style.display = 'none';
+}
